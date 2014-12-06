93d8ca9ebc9b882b013735f7f9b7c0f4d64070b7
Azman M. Yusof <azman@my1matrix.org>
2014-12-06 21:10:36 +0800

    Initial Commit
    - simple files (clearly separated asm & c version)

---
+++ my1barepi0_intro2c/main.c
@@ -0,0 +1,28 @@
+#define GPIO_BASE 0x20200000
+#define GPIO_FSEL 0x00
+#define GPIO_FSET 0x07
+#define GPIO_FCLR 0x0A
+
+#define GPIO_ACT_LED 47
+
+volatile unsigned int *gpio;
+volatile unsigned int loop;
+
+void main(void)
+{
+	/** point to gpio access register */
+	gpio = (unsigned int*) GPIO_BASE;
+	/** configure gpio as output */
+	gpio[GPIO_FSEL+(GPIO_ACT_LED/10)] = 1 << (GPIO_ACT_LED%10)*3;
+	while(1)
+	{
+		/** clear pin - on led! */
+		gpio[GPIO_FCLR+(GPIO_ACT_LED/32)] = 1 << (GPIO_ACT_LED%32);
+		/** delay a bit to allow us see the light! */
+		for(loop=0;loop<0x3F0000;loop++);
+		/** set pin - off led! */
+		gpio[GPIO_FSET+(GPIO_ACT_LED/32)] = 1 << (GPIO_ACT_LED%32);
+		/** delay a bit to allow us see the light! */
+		for(loop=0;loop<0x3F0000;loop++);
+	}
+}
