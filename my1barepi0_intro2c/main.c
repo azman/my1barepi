d1516104bd39f38c11a89c27b6b2069ad6fa5472
Azman M. Yusof <azman@my1matrix.net>
2015-12-22 13:31:04 +0800

    Added proper comments to tutorial 0
    - slightly modified makefile...

--- my1barepi0_intro2c/main.c
+++ my1barepi0_intro2c/main.c
@@ -2,11 +2,11 @@
 #define GPIO_FSEL 0x00
 #define GPIO_FSET 0x07
 #define GPIO_FCLR 0x0A
-
+/** GPIO number for ACT LED on R-Pi Model B+ v1.2 */
 #define GPIO_ACT_LED 47
 
-volatile unsigned int *gpio;
-volatile unsigned int loop;
+/** volatile coz -O2 compiler option would otherwise kill them? */
+volatile unsigned int *gpio, loop;
 
 void main(void)
 {
@@ -14,15 +14,16 @@ void main(void)
 	gpio = (unsigned int*) GPIO_BASE;
 	/** configure gpio as output */
 	gpio[GPIO_FSEL+(GPIO_ACT_LED/10)] = 1 << (GPIO_ACT_LED%10)*3;
+	/** main loop */
 	while(1)
 	{
 		/** clear pin - on led! */
 		gpio[GPIO_FCLR+(GPIO_ACT_LED/32)] = 1 << (GPIO_ACT_LED%32);
 		/** delay a bit to allow us see the light! */
-		for(loop=0;loop<0x3F0000;loop++);
+		for(loop=0;loop<0x200000;loop++);
 		/** set pin - off led! */
 		gpio[GPIO_FSET+(GPIO_ACT_LED/32)] = 1 << (GPIO_ACT_LED%32);
 		/** delay a bit to allow us see the blink! */
-		for(loop=0;loop<0x3F0000;loop++);
+		for(loop=0;loop<0x200000;loop++);
 	}
 }
