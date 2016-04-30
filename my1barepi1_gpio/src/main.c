5584b25074a328db773a63959b963cc737b13987
Azman M. Yusof <azman@my1matrix.net>
2016-04-30 23:01:45 +0800

    Updated info in README for tutorial 1
    - gpio module integrated back into main
      = only 'finalized' code will be kept as modules
      = stored in my1barepiZ

--- my1barepi1_gpio/src/main.c
+++ my1barepi1_gpio/src/main.c
@@ -1,5 +1,55 @@
 /*----------------------------------------------------------------------------*/
-#include "gpio.h"
+/** GPIO MODULE BEGIN - THESE FUNCTIONS WILL BE MADE INTO A LIBRARY! */
+/*----------------------------------------------------------------------------*/
+#ifdef RASPI2
+#define GPIO_BASE 0x3F200000
+#else
+#define GPIO_BASE 0x20200000
+#endif
+#define GPIO_FSEL 0x00
+#define GPIO_FSET 0x07
+#define GPIO_FCLR 0x0A
+#define GPIO_FGET 0x0D
+/*----------------------------------------------------------------------------*/
+#define GPIO_SELECT_BITS 3
+#define GPIO_SELECT 0x07
+/*----------------------------------------------------------------------------*/
+#define GPIO_INPUT  0x00
+#define GPIO_OUTPUT 0x01
+/*----------------------------------------------------------------------------*/
+volatile unsigned int *gpio;
+/*----------------------------------------------------------------------------*/
+void gpio_init(void)
+{
+	gpio = (unsigned int*) GPIO_BASE;
+}
+/*----------------------------------------------------------------------------*/
+void gpio_config(int gpio_num, int gpio_sel)
+{
+	unsigned int shift = (gpio_num%10)*GPIO_SELECT_BITS;
+	unsigned int index = (gpio_num/10)+GPIO_FSEL;
+	unsigned int mask = GPIO_SELECT << shift;
+	unsigned int value = gpio_sel << shift;
+	gpio[index] &= ~mask;
+	gpio[index] |= value;
+}
+/*----------------------------------------------------------------------------*/
+void gpio_set(int gpio_num)
+{
+	gpio[GPIO_FSET+(gpio_num/32)] = 1 << (gpio_num%32);
+}
+/*----------------------------------------------------------------------------*/
+void gpio_clr(int gpio_num)
+{
+	gpio[GPIO_FCLR+(gpio_num/32)] = 1 << (gpio_num%32);
+}
+/*----------------------------------------------------------------------------*/
+unsigned int gpio_read(int gpio_num)
+{
+	return gpio[GPIO_FGET+(gpio_num/32)] & (1<<(gpio_num%32));
+}
+/*----------------------------------------------------------------------------*/
+/** GPIO MODULE END */
 /*----------------------------------------------------------------------------*/
 #define MY_LED 2
 #define MY_SWITCH 3
@@ -8,7 +58,11 @@
 #define LED_OFF gpio_set
 #define SWITCH_IS_OFF gpio_read
 /*----------------------------------------------------------------------------*/
+#ifdef RASPI2
 #define COUNT_MAX 0x100000
+#else
+#define COUNT_MAX 0x200000
+#endif
 /*----------------------------------------------------------------------------*/
 void main(void)
 {
