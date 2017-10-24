a2b8618a302d533c3fcf3a20f3f904f93d7326f5
Azman M. Yusof <azman@my1matrix.net>
2017-10-24 09:37:33 +0800

    Updated playground...
    - now interrupt-ready and floating-point-ready :p
    - will be the template for next session

--- tZZ_modules/src/fpu.s
+++ tZZ_modules/src/fpu.s
@@ -1,9 +1,10 @@
 .section .text
 .global enable_fpu
 enable_fpu:
+@ from http://wiki.osdev.org/ARM_RaspberryPi#Floating_point_support_-_VFP
 	ldr r0, =(0xF << 20)
 	mcr p15, 0, r0, c1, c0, 2
 	mov r3, #0x40000000
-@  vmsr fpexc, r3    # assembler bug? says not supported :(
+@	vmsr fpexc, r3    # assembler bug? says not supported :(
 	.long 0xeee83a10
 	mov	pc, lr
