2fb067510f68f90f80f510fca9173900b1e9b31e
Azman M. Yusof <azman@my1matrix.net>
2017-10-27 16:21:52 +0800

    Added text LCD as primary display for gsm tutorial
    - uartbb maintained as debug terminal
    - will rewrite this to use graphics display instead

--- t10_gsm/src/main.c
+++ t10_gsm/src/main.c
@@ -3,6 +3,7 @@
 #include "uart.h"
 #include "timer.h"
 #include "uartbb.h"
+#include "textlcd.h"
 #include "gsm.h"
 #include "string.h"
 /*----------------------------------------------------------------------------*/
@@ -45,25 +46,40 @@ void do_check(char* buffer, int count)
 	if (check) do_debug(0x0,buffer,check);
 }
 /*----------------------------------------------------------------------------*/
+void do_print(char* msg, int rst)
+{
+	int loop;
+	if (rst)
+	{
+		/* remove any previous char? */
+		lcd_send_command(LCD_POS_LINE2);
+		for (loop=0;loop<LCD_MAX_CHAR;loop++)
+			lcd_send_data(' ');
+		lcd_send_command(LCD_POS_LINE2);
+	}
+	lcd_print(msg);
+}
+/*----------------------------------------------------------------------------*/
 void do_abort(void)
 {
 	uartbb_print("ABORT!");
+	do_print("#",0);
 	while(1);
 }
 /*----------------------------------------------------------------------------*/
 void main(void)
 {
 	unsigned int initc = 0, okchk;
-	int count = 0;
+	int count = 0, reset = 1;
 	char buffer[BUFF_SIZE], *pbuff;
 	/** initialize stuffs */
 	gpio_init();
 	gpio_config(MESG_GPIO,GPIO_INPUT);
-	gpio_setevent(MESG_GPIO,GPIO_EVENT_AEDGF);
+	gpio_setevent(MESG_GPIO,GPIO_EVENT_AEDGR);
 	gpio_config(CALL_GPIO,GPIO_INPUT);
-	gpio_setevent(CALL_GPIO,GPIO_EVENT_AEDGF);
+	gpio_setevent(CALL_GPIO,GPIO_EVENT_AEDGR);
 	gpio_config(HANG_GPIO,GPIO_INPUT);
-	gpio_setevent(HANG_GPIO,GPIO_EVENT_AEDGF);
+	gpio_setevent(HANG_GPIO,GPIO_EVENT_AEDGR);
 	timer_init();
 	/** initialize uart with default baudrate */
 	uart_init(UART_BAUD_DEFAULT);
@@ -71,27 +87,38 @@ void main(void)
 	/** send out the word! */
 	uartbb_print("\n\n");
 	uartbb_print("YAY! It is alive!\n");
+	/** update lcd! */
+	lcd_init();
+	lcd_send_command(LCD_POS_LINE1);
+	lcd_print("MY1GSM INTERFACE");
 	/* flush incoming uart buffer */
 	uart_purge();
 	/* looking for modem response */
 	while(1)
 	{
 		uartbb_print("Sending 'AT'... ");
+		do_print("Initializing: ",1);
 		gsm_command("AT");
 		count = gsm_replies(buffer,BUFF_SIZE,&okchk);
 		if (count==0)
 		{
 			uartbb_print("no response.\n");
+			do_print("??",0);
 		}
 		else
 		{
 			if (okchk==GSM_OK_COMPLETE)
 			{
 				uartbb_print("OK.\n");
+				do_print("OK",0);
 				break;
 			}
-			/** else do_debug("invalid response.\n",buffer,count); */
-			else uartbb_print("invalid response.\n");
+			else
+			{
+				/** do_debug("invalid response.\n",buffer,count); */
+				uartbb_print("invalid response.\n");
+				do_print("KO",0);
+			}
 		}
 		timer_wait(GSM_WAIT_RETRY); /* wait between retries */
 	}
@@ -99,43 +126,69 @@ void main(void)
 	do_check(buffer,BUFF_SIZE);
 	/* try to disable echo - tc35 needs this */
 	uartbb_print("Sending 'ATE'... ");
+	do_print("Send 'ATE': ",1);
 	gsm_command("ATE");
 	count = gsm_replies(buffer,BUFF_SIZE,&okchk);
 	if (count==0)
 	{
 		uartbb_print("no response.\n");
+		do_print("??",0);
 		do_abort();
 	}
 	else
 	{
-		if (okchk==GSM_OK_COMPLETE) uartbb_print("OK.\n");
-		else do_debug("invalid response.\n",buffer,count);
+		if (okchk==GSM_OK_COMPLETE)
+		{
+			uartbb_print("OK.\n");
+			do_print("OK",0);
+		}
+		else
+		{
+			do_debug("invalid response.\n",buffer,count);
+			do_abort();
+		}
 	}
 	do_check(buffer,BUFF_SIZE);
 	/* on sim908, must send ate 0 */
 	uartbb_print("Sending 'ATE 0'... ");
+	do_print("Send 'ATE 0': ",1);
 	gsm_command("ATE 0");
 	count = gsm_replies(buffer,BUFF_SIZE,&okchk);
 	if (count==0)
 	{
 		uartbb_print("no response.\n");
+		do_print("??",0);
 		do_abort();
 	}
 	else
 	{
 		pbuff = gsm_trim_prefix(buffer);
-		if (okchk==GSM_OK_COMPLETE) uartbb_print("OK.\n");
-		else if (!strncmp(pbuff,"ERROR",5)) uartbb_print("ignored.\n");
-		else do_debug("invalid response.\n",buffer,count);
+		if (okchk==GSM_OK_COMPLETE)
+		{
+			uartbb_print("OK.\n");
+			do_print("OK",0);
+		}
+		else if (!strncmp(pbuff,"ERROR",5))
+		{
+			uartbb_print("ignored.\n");
+			do_print("%%",0);
+		}
+		else
+		{
+			do_debug("invalid response.\n",buffer,count);
+			do_abort();
+		}
 	}
 	do_check(buffer,BUFF_SIZE);
 	/* check if sim card is in */
 	uartbb_print("Checking SIM status... ");
+	do_print("SIM: ",1);
 	gsm_command("AT+CPIN?");
 	count = gsm_replies(buffer,BUFF_SIZE,&okchk);
 	if (count==0)
 	{
 		uartbb_print("no response.\n");
+		do_print("??",0);
 		do_abort();
 	}
 	else
@@ -144,10 +197,12 @@ void main(void)
 		if (!strncmp(pbuff,"+CPIN: READY",12))
 		{
 			uartbb_print("SIM ready.\n");
+			do_print("Ready",0);
 		}
 		else if (!strncmp(pbuff,"ERROR",5))
 		{
 			uartbb_print("missing SIM?\n");
+			do_print("??",0);
 			do_abort();
 		}
 		else
@@ -159,23 +214,28 @@ void main(void)
 	do_check(buffer,BUFF_SIZE);
 	/* setting text mode for sending sms */
 	uartbb_print("Setting SMS Text Mode... ");
+	do_print("SMS Mode: ",1);
 	gsm_command("AT+CMGF=1");
 	count = gsm_replies(buffer,BUFF_SIZE,&okchk);
 	if (count==0)
 	{
 		uartbb_print("no response.\n");
+		do_print("??",0);
 		do_abort();
 	}
 	else
 	{
-		if (okchk==GSM_OK_COMPLETE) uartbb_print("OK.\n");
+		if (okchk==GSM_OK_COMPLETE)
+		{
+			uartbb_print("OK.\n");
+			do_print("OK",0);
+		}
 		else
 		{
 			do_debug("invalid response.\n",buffer,count);
 			do_abort();
 		}
 	}
-	uartbb_print("GSM Module Ready for Call/SMS!\n");
 	/** clear invalid gpio events - just in case */
 	gpio_rstevent(MESG_GPIO);
 	gpio_rstevent(CALL_GPIO);
@@ -183,20 +243,54 @@ void main(void)
 	/** main loop */
 	while(1)
 	{
-		do_check(buffer,BUFF_SIZE);
+		if (reset)
+		{
+			uartbb_print("GSM Module Ready for Call/SMS!\n");
+			do_print("Module Ready",1);
+			reset = 0;
+		}
+		/* any news? */
+		if (uart_incoming())
+		{
+			count = gsm_replies(buffer,count,0x0);
+			if (count)
+			{
+				do_debug(0x0,buffer,check);
+				do_print("@@",1);
+				do_print(buffer,0);
+				timer_wait(3*TIMER_S);
+				reset = 1;
+			}
+		}
+		/* check requests */
 		if (gpio_chkevent(MESG_GPIO))
 		{
 			/** send the thing... */
 			uartbb_print("Sending SMS to "PHONENUM"... ");
+			do_print("SMS:"PHONENUM,1);
 			gsm_command("AT+CMGS=\""PHONENUM"\"");
 			while(uart_read()!='>');
 			uart_print("HELLO, WORLD!");
 			uart_send(0x1a); // ctrl+z
 			timer_wait(1*TIMER_S); // got one whitespace char on tc35?
 			count = gsm_replies(buffer,BUFF_SIZE,&okchk);
-			if (count==0) uartbb_print("no response?\n");
-			else if (okchk==GSM_OK_COMPLETE) uartbb_print("OK.\n");
-			else do_debug("invalid response.\n",buffer,count);
+			if (count==0)
+			{
+				uartbb_print("no response?\n");
+				do_print("SMS:??",1);
+			}
+			else if (okchk==GSM_OK_COMPLETE)
+			{
+				uartbb_print("OK.\n");
+				do_print("SMS:OK",1);
+			}
+			else
+			{
+				do_debug("invalid response.\n",buffer,count);
+				do_print("SMS:KO",1);
+				timer_wait(3*TIMER_S);
+				reset = 1;
+			}
 			gpio_rstevent(MESG_GPIO);
 			gpio_rstevent(CALL_GPIO); /* cannot request call while messaging */
 			gpio_rstevent(HANG_GPIO);
@@ -207,6 +301,7 @@ void main(void)
 			gpio_rstevent(HANG_GPIO);
 			/** make a call... */
 			uartbb_print("Calling "PHONENUM"... ");
+			do_print(">>>:"PHONENUM,1);
 			gsm_command("ATD"PHONENUM";");
 			initc = timer_read();
 			while(timer_read()-initc<CALL_RING_DELAY)
@@ -214,6 +309,7 @@ void main(void)
 				if (gpio_chkevent(HANG_GPIO))
 				{
 					uartbb_print("\nHanging up... ");
+					do_print(">>>:HangUp",1);
 					gsm_command("ATH");
 					count = gsm_replies(buffer,BUFF_SIZE,&okchk);
 					if (count)
@@ -222,11 +318,20 @@ void main(void)
 						if (!strncmp(pbuff,"NO CARRIER",10))
 						{
 							uartbb_print("done!\n");
+							do_print("$",0);
 							break;
 						}
-						else do_debug("huh?\n",buffer,count);
+						else
+						{
+							do_debug("huh?\n",buffer,count);
+							do_print("*",0);
+						}
 					}
-					else uartbb_print("\n");
+					else
+					{
+						uartbb_print("\n");
+						do_print("??",0);
+					}
 					break;
 				}
 				if (uart_incoming())
@@ -236,7 +341,14 @@ void main(void)
 					if (!strncmp(pbuff,"NO CARRIER",10))
 					{
 						uartbb_print("call rejected?\n");
+						do_print("??",0);
 						break;
+					}
+					else
+					{
+						do_print(pbuff,1);
+						timer_wait(3*TIMER_S);
+						reset = 1;
 					}
 				}
 			}
@@ -245,6 +357,6 @@ void main(void)
 			gpio_rstevent(MESG_GPIO); /* cannot do messaging while calling */
 			continue;
 		}
-	}
+
 }
 /*----------------------------------------------------------------------------*/
