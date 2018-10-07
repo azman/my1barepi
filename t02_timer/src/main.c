58fbe9f7624b267d5220b7fe75a8d1e877402196
Azman M. Yusof <azman@my1matrix.net>
2018-10-07 16:46:56 +0800

    Made t02 optimized again...
    - removed timer-specific codes - using timer module library

--- t02_timer/src/main.c
+++ t02_timer/src/main.c
@@ -1,44 +1,6 @@
 /*----------------------------------------------------------------------------*/
 #include "gpio.h"
-/*----------------------------------------------------------------------------*/
-/** TIMER MODULE BEGIN - THESE FUNCTIONS WILL BE MADE INTO A LIBRARY! */
-/*----------------------------------------------------------------------------*/
-#include "raspi.h"
-/*----------------------------------------------------------------------------*/
-#define TIMER_BASE (PMAP_BASE|TIMER_OFFSET)
-#define TIMER_CTRL 0x02
-#define TIMER_CVAL 0x08
-/*----------------------------------------------------------------------------*/
-/** free-running counter enable bit position */
-#define TIMER_FRC_ENABLE 0x00000200
-/*----------------------------------------------------------------------------*/
-/* timer_wait is usleep equivalent */
-#define TIMER_US 1
-#define TIMER_MS 1000
-#define TIMER_S 1000000
-/*----------------------------------------------------------------------------*/
-volatile unsigned int *timer;
-/*----------------------------------------------------------------------------*/
-void timer_init(void)
-{
-	timer = (unsigned int*) TIMER_BASE;
-	/** prescaler = 0xF9, freq=sys_clk/(prescaler+1)*/
-	timer[TIMER_CTRL] = 0x00F90000; /** for 1MHz? sys_clk is 250MHz? */
-	timer[TIMER_CTRL] |= TIMER_FRC_ENABLE;
-}
-/*----------------------------------------------------------------------------*/
-unsigned int timer_read(void)
-{
-	return timer[TIMER_CVAL];
-}
-/*----------------------------------------------------------------------------*/
-void timer_wait(unsigned int wait)
-{
-	unsigned int init = timer_read();
-	while((timer_read()-init)<wait);
-}
-/*----------------------------------------------------------------------------*/
-/** TIMER MODULE END */
+#include "timer.h"
 /*----------------------------------------------------------------------------*/
 #define TEST_PIN 2
 #define TEST_DELAY 500
