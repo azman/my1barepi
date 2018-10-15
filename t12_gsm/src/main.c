e905b80e2262e48b7da25289bba36eb86877ce1e
Azman M. Yusof <azman@my1matrix.net>
2018-10-15 12:18:20 +0800

    GSM working - cannot be optimized because using video module

--- t12_gsm/src/main.c
+++ t12_gsm/src/main.c
@@ -2,8 +2,6 @@
 #include "gpio.h"
 #include "timer.h"
 #include "uart.h"
-#include "uartbb.h"
-#include "textlcd.h"
 #include "gsm.h"
 #include "string.h"
 #include "mailbox.h"
@@ -23,27 +21,17 @@
 /*----------------------------------------------------------------------------*/
 void debug_hexbyte(unsigned char byte)
 {
-	unsigned char temp = (byte & 0xf0) >> 4;
-	if(temp>9) temp = (temp-10)+0x41;
-	else temp += 0x30;
-	uartbb_send(temp);
-	video_text_char(temp);
-	temp = (byte & 0x0f);
-	if(temp>9) temp = (temp-10)+0x41;
-	else temp += 0x30;
-	uartbb_send(temp);
-	video_text_char(temp);
+	video_text_char(byte2hex(byte,1,0));
+	video_text_char(byte2hex(byte,0,0));
 }
 /*----------------------------------------------------------------------------*/
 void debug_char(char chk)
 {
-	uartbb_send(chk);
 	video_text_char(chk);
 }
 /*----------------------------------------------------------------------------*/
 void debug_print(char *msg)
 {
-	uartbb_print(msg);
 	video_text_string(msg);
 }
 /*----------------------------------------------------------------------------*/
@@ -74,24 +62,9 @@ void do_check(char* buffer, int count)
 	if (check) do_debug(0x0,buffer,check);
 }
 /*----------------------------------------------------------------------------*/
-void do_print(char* msg, int rst)
-{
-	int loop;
-	if (rst)
-	{
-		/* remove any previous char? */
-		lcd_send_command(LCD_POS_LINE2);
-		for (loop=0;loop<LCD_MAX_CHAR;loop++)
-			lcd_send_data(' ');
-		lcd_send_command(LCD_POS_LINE2);
-	}
-	lcd_print(msg);
-}
-/*----------------------------------------------------------------------------*/
 void do_abort(void)
 {
 	debug_print("ABORT!");
-	do_print("#",0);
 	while(1);
 }
 /*----------------------------------------------------------------------------*/
@@ -114,44 +87,35 @@ void main(void)
 	timer_init();
 	/** initialize uart with default baudrate */
 	uart_init(UART_BAUD_DEFAULT);
-	uartbb_init(UARTBB_RX_DEFAULT,UARTBB_TX_DEFAULT);
 	/** initialize video stuff - mailbox before that! */
 	mailbox_init();
-	video_init();
+	video_init(VIDEO_RES_VGA);
 	video_clear();
 	/** send out the word! */
 	debug_print("\n\nYAY! It is alive!\n");
-	/** update lcd! */
-	lcd_init();
-	lcd_send_command(LCD_POS_LINE1);
-	lcd_print("MY1GSM INTERFACE");
 	/* flush incoming uart buffer */
 	uart_purge();
 	/* looking for modem response */
 	while(1)
 	{
 		debug_print("Sending 'AT'... ");
-		do_print("Initializing: ",1);
 		gsm_command("AT");
 		count = gsm_replies(buffer,BUFF_SIZE,&okchk);
 		if (count==0)
 		{
 			debug_print("no response.\n");
-			do_print("??",0);
 		}
 		else
 		{
 			if (okchk==GSM_OK_COMPLETE)
 			{
 				debug_print("OK.\n");
-				do_print("OK",0);
 				break;
 			}
 			else
 			{
 				/** do_debug("invalid response.\n",buffer,count); */
 				debug_print("invalid response.\n");
-				do_print("KO",0);
 			}
 		}
 		timer_wait(GSM_WAIT_RETRY); /* wait between retries */
@@ -160,13 +124,11 @@ void main(void)
 	do_check(buffer,BUFF_SIZE);
 	/* try to disable echo - tc35 needs this */
 	debug_print("Sending 'ATE'... ");
-	do_print("Send 'ATE': ",1);
 	gsm_command("ATE");
 	count = gsm_replies(buffer,BUFF_SIZE,&okchk);
 	if (count==0)
 	{
 		debug_print("no response.\n");
-		do_print("??",0);
 		do_abort();
 	}
 	else
@@ -174,7 +136,6 @@ void main(void)
 		if (okchk==GSM_OK_COMPLETE)
 		{
 			debug_print("OK.\n");
-			do_print("OK",0);
 		}
 		else
 		{
@@ -185,13 +146,11 @@ void main(void)
 	do_check(buffer,BUFF_SIZE);
 	/* on sim908, must send ate 0 */
 	debug_print("Sending 'ATE 0'... ");
-	do_print("Send 'ATE 0': ",1);
 	gsm_command("ATE 0");
 	count = gsm_replies(buffer,BUFF_SIZE,&okchk);
 	if (count==0)
 	{
 		debug_print("no response.\n");
-		do_print("??",0);
 		do_abort();
 	}
 	else
@@ -200,12 +159,10 @@ void main(void)
 		if (okchk==GSM_OK_COMPLETE)
 		{
 			debug_print("OK.\n");
-			do_print("OK",0);
 		}
 		else if (!strncmp(pbuff,"ERROR",5))
 		{
 			debug_print("ignored.\n");
-			do_print("%%",0);
 		}
 		else
 		{
@@ -216,13 +173,11 @@ void main(void)
 	do_check(buffer,BUFF_SIZE);
 	/* check if sim card is in */
 	debug_print("Checking SIM status... ");
-	do_print("SIM: ",1);
 	gsm_command("AT+CPIN?");
 	count = gsm_replies(buffer,BUFF_SIZE,&okchk);
 	if (count==0)
 	{
 		debug_print("no response.\n");
-		do_print("??",0);
 		do_abort();
 	}
 	else
@@ -231,12 +186,10 @@ void main(void)
 		if (!strncmp(pbuff,"+CPIN: READY",12))
 		{
 			debug_print("SIM ready.\n");
-			do_print("Ready",0);
 		}
 		else if (!strncmp(pbuff,"ERROR",5))
 		{
 			debug_print("missing SIM?\n");
-			do_print("??",0);
 			do_abort();
 		}
 		else
@@ -248,13 +201,11 @@ void main(void)
 	do_check(buffer,BUFF_SIZE);
 	/* setting text mode for sending sms */
 	debug_print("Setting SMS Text Mode... ");
-	do_print("SMS Mode: ",1);
 	gsm_command("AT+CMGF=1");
 	count = gsm_replies(buffer,BUFF_SIZE,&okchk);
 	if (count==0)
 	{
 		debug_print("no response.\n");
-		do_print("??",0);
 		do_abort();
 	}
 	else
@@ -262,7 +213,6 @@ void main(void)
 		if (okchk==GSM_OK_COMPLETE)
 		{
 			debug_print("OK.\n");
-			do_print("OK",0);
 		}
 		else
 		{
@@ -280,7 +230,6 @@ void main(void)
 		if (reset)
 		{
 			debug_print("GSM Module Ready for Call/SMS!\n");
-			do_print("Module Ready",1);
 			reset = 0;
 		}
 		/* any news? */
@@ -290,8 +239,6 @@ void main(void)
 			if (count)
 			{
 				do_debug(0x0,buffer,count);
-				do_print("@@",1);
-				do_print(buffer,0);
 				timer_wait(3*TIMER_S);
 				reset = 1;
 			}
@@ -301,7 +248,6 @@ void main(void)
 		{
 			/** send the thing... */
 			debug_print("Sending SMS to "PHONENUM"... ");
-			do_print("SMS:"PHONENUM,1);
 			gsm_command("AT+CMGS=\""PHONENUM"\"");
 			while(uart_read()!='>');
 			uart_print("HELLO, WORLD!");
@@ -311,17 +257,14 @@ void main(void)
 			if (count==0)
 			{
 				debug_print("no response?\n");
-				do_print("SMS:??",1);
 			}
 			else if (okchk==GSM_OK_COMPLETE)
 			{
 				debug_print("OK.\n");
-				do_print("SMS:OK",1);
 			}
 			else
 			{
 				do_debug("invalid response.\n",buffer,count);
-				do_print("SMS:KO",1);
 				timer_wait(3*TIMER_S);
 				reset = 1;
 			}
@@ -335,7 +278,6 @@ void main(void)
 			gpio_rstevent(HANG_GPIO);
 			/** make a call... */
 			debug_print("Calling "PHONENUM"... ");
-			do_print(">>>:"PHONENUM,1);
 			gsm_command("ATD"PHONENUM";");
 			initc = timer_read();
 			while(timer_read()-initc<CALL_RING_DELAY)
@@ -343,7 +285,6 @@ void main(void)
 				if (gpio_chkevent(HANG_GPIO))
 				{
 					debug_print("\nHanging up... ");
-					do_print(">>>:HangUp",1);
 					gsm_command("ATH");
 					count = gsm_replies(buffer,BUFF_SIZE,&okchk);
 					if (count)
@@ -352,19 +293,16 @@ void main(void)
 						if (!strncmp(pbuff,"NO CARRIER",10))
 						{
 							debug_print("done!\n");
-							do_print("$",0);
 							break;
 						}
 						else
 						{
 							do_debug("huh?\n",buffer,count);
-							do_print("*",0);
 						}
 					}
 					else
 					{
 						debug_print("\n");
-						do_print("??",0);
 					}
 					break;
 				}
@@ -375,12 +313,10 @@ void main(void)
 					if (!strncmp(pbuff,"NO CARRIER",10))
 					{
 						debug_print("call rejected?\n");
-						do_print("??",0);
 						break;
 					}
 					else
 					{
-						do_print(pbuff,1);
 						timer_wait(3*TIMER_S);
 						reset = 1;
 					}
