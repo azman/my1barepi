54ef8bf2eda60ad72bababc94b44e0cd6aea5ef9
Azman M. Yusof <azman@my1matrix.org>
2019-12-11 11:57:19 +0800

    Code clean-up...
    - also added info for t00_intro4u
      = will use this to teach basic c access for gpio

--- tZZ_modules/src/boot.h
+++ tZZ_modules/src/boot.h
@@ -2,9 +2,9 @@
 #ifndef __MY1BOOT_H__
 #define __MY1BOOT_H__
 /*----------------------------------------------------------------------------*/
-unsigned int get32(unsigned int);
-void put32(unsigned int,unsigned int);
-void loopd(unsigned int);
+unsigned int get32(unsigned int addr);
+void put32(unsigned int addr,unsigned int data);
+void loopd(unsigned int wait);
 /*----------------------------------------------------------------------------*/
 #endif
 /*----------------------------------------------------------------------------*/
--- tZZ_modules/src/boot.s
+++ tZZ_modules/src/boot.s
@@ -1,26 +1,23 @@
-#-------------------------------------------------------------------------------
 .section .boot
 boot:
 	mov sp,#0x00008000
 	bl main
 here:
 	b here
-#-------------------------------------------------------------------------------
+@ read 32-bit data from the given 32-bit address
 .global get32
 get32:
 	ldr r0,[r0]
 	bx lr
-#-------------------------------------------------------------------------------
+@ write 32-bit data to the given 32-bit address
 .global put32
 put32:
 	str r1,[r0]
 	bx lr
-#-------------------------------------------------------------------------------
-@loop delay
+@ loop delay: count {wait} time(s)
 .global loopd
 loopd:
 	sub r0,#1
 	cmp r0,#0
 	bne loopd
 	bx lr
-#-------------------------------------------------------------------------------
