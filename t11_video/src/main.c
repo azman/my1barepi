a4a127c6df63012199b18a621dcaf9dbfebe211d
Azman M. Yusof <azman@my1matrix.org>
2020-10-10 17:54:50 +0800

    Cleanup...

--- t11_video/src/main.c
+++ t11_video/src/main.c
@@ -12,7 +12,7 @@ void main(void)
 	int count = 0;
 	gpio_config(GPIO_TEST,GPIO_OUTPUT);
 	gpio_clr(GPIO_TEST);
-	video_init(VIDEO_RES_MAX);
+	video_init(VIDEO_RES_MAX); /** or, VIDEO_RES_VGA */
 	video_set_bgcolor(COLOR_BLUE);
 	video_clear();
 	video_text_cursor(1,1);
@@ -21,6 +21,10 @@ void main(void)
 	video_text_string("Count: ");
 	int2str(buff,count);
 	video_text_string(buff);
+	/* test line drawing */
+	video_draw_line(100,100,200,100);
+	video_draw_line(200,100,100,200);
+	video_draw_line(100,200,100,100);
 	while (1)
 	{
 		loopd(0x200000);
