c54f389dd8ee8091081cdeb5c8eaf59995b83048
Azman M. Yusof <azman@sauron.local>
2020-10-20 16:16:01 +0800

    Added sysflag area in boot-interrupt.s

--- tZZ_modules/src/boot-interrupt.s
+++ tZZ_modules/src/boot-interrupt.s
@@ -3,6 +3,7 @@
 .section .boot
 boot:
 .endif
+that:
 @ these are actually jump instructions
 	ldr pc,reset_handler
 	ldr pc,undef_handler
@@ -21,7 +22,22 @@ edata_handler: .word here
 resvd_handler: .word here
 doirq_handler: .word irqh
 dofiq_handler: .word here
+@ system flag starts with "MY1\0"
+sys0flag: .word 0x0031594D
+sys1flag: .word 0
+sys2flag: .word 0
+sys3flag: .word 0
+sys4flag: .word 0
+sys5flag: .word 0
+sys6flag: .word 0
+sys7flag: .word 0
 init:
+@ will skip interrupt vector table if one is already there...
+	ldr r0,=0x0000
+	ldr r0,[r0,#((sys0flag-that)<<2)]
+	ldr r1,sys0flag
+	cmp r0,r1
+	beq next
 @ moving (overriding) interrupt vector table
 	mov r0,#0x8000
 	mov r1,#0x0000
@@ -31,11 +47,34 @@ init:
 @ copying the jmp targets (hint: r0 & r1 incremented!)
 	ldmia r0!,{r2,r3,r4,r5,r6,r7,r8,r9}
 	stmia r1!,{r2,r3,r4,r5,r6,r7,r8,r9}
+next:
 @ continue with the program
 	b load
 .section .load
 load:
 .include "boot.s"
+@ get system flag
+.global get_sysflag
+get_sysflag:
+	ldr r1,=0x0000
+	cmp r0,#8
+	bge get_sysflag_done
+	mov r0,r0,lsl #2
+	add r0,r0,#(sys0flag-that)
+	ldr r1,[r0]
+get_sysflag_done:
+	mov r0,r1
+	bx lr
+@ set system flag
+.global set_sysflag
+set_sysflag:
+	cmp r0,#8
+	bge set_sysflag_done
+	mov r0,r0,lsl #2
+	add r0,r0,#(sys0flag-that)
+	str r1,[r0]
+set_sysflag_done:
+	bx lr
 @ duh! call this to enable interrupt
 .global enable_irq
 enable_irq:
